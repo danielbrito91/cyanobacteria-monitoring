9edd84f79307ae20f00d8a09d5d3889d489ea405
danielbrito91 <danieloliveiradebrito@gmail.com>
2022-09-13 16:23:00 -0300

    MLFlow: primeira config

--- src/data/load_vigilancia.py
+++ src/data/load_vigilancia.py
@@ -4,16 +4,28 @@ import yaml
 from typing import Text
 import pandas as pd
 
+from src.utils import logs
+
+def check_vigilancia_date():
+    pass
+
 def load_vigilancia(config_path: Text) -> pd.DataFrame:
     """Realiza a leitura de dados baixados da Vigilancia para um dado manancial"""
+
+    logger = logs.get_logger("DATA_LABEL", log_level="WARNING")
 
     with open(config_path) as config_file:
         config = yaml.safe_load(config_file)
 
     if not os.path.isfile(config["data_load"]["labels_download"]):
-        urllib.request.urlretrieve(
-            config["data_create"]["url"],
-            config["data_load"]["labels_download"])
+        try:    
+            urllib.request.urlretrieve(
+                config["data_create"]["url"],
+                config["data_load"]["labels_download"])
+        except Exception as e:
+            logger.exeption(
+                "Unable to download data from vigilancia"
+            )
 
     vigilancia = pd.read_csv(config["data_load"]["labels_download"],
            compression="zip",
--- src/train/train_model.py
+++ src/train/train_model.py
@@ -7,6 +7,11 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import PowerTransformer
 from typing import Dict, Text
 from xgboost import XGBRegressor
+from urllib.parse import urlparse
+
+import mlflow
+import mlflow.xgboost
+import mlflow.sklearn
 
 class UnsupportedRegressor(Exception):
 
@@ -23,26 +28,45 @@ def get_supported_estimator() -> Dict:
 
 def train_model(df: pd.DataFrame, target_column: Text, 
     estimator_name: Text, params: Dict, polynomial_degree: int):
-    
+
     estimators = get_supported_estimator()
     if estimator_name not in estimators.keys():
         raise UnsupportedRegressor(estimator_name)
-    
-    regressor = estimators[estimator_name](**params)
-    poly = PolynomialFeatures(degree=polynomial_degree, include_bias=False)
-    poly_reg = Pipeline([
-            ("poly", poly),
-            ("regressor", regressor)
-        ])
-    
-    model = TransformedTargetRegressor(
-        regressor = poly_reg,
-        transformer=PowerTransformer(method="yeo-johnson")
-    )
 
-    # Get X and y
-    X_train = df.drop(columns = [target_column])
-    y_train = df[target_column]
-    model.fit(X_train, y_train)
+    # Start MLFlow
+    with mlflow.start_run():
+        regressor = estimators[estimator_name](**params)
+        poly = PolynomialFeatures(degree=polynomial_degree, include_bias=False)
+        poly_reg = Pipeline([
+                ("poly", poly),
+                ("regressor", regressor)
+            ])
+        
+        model = TransformedTargetRegressor(
+            regressor = poly_reg,
+            transformer=PowerTransformer(method="yeo-johnson")
+        )
 
+        # Get X and y
+        X_train = df.drop(columns = [target_column])
+        y_train = df[target_column]
+
+        model.fit(X_train, y_train)
+
+        for param, value in params.items():
+            mlflow.log_param(param, value)
+        
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            if estimator_name !=  "xgboost":
+                mlflow.sklearn.log_model(model, "model", registered_model_name=estimator_name)
+            else:
+                mlflow.xgboost.log_model(model, "model", registered_model_name=estimator_name)
+        else:
+            if estimator_name !=  "xgboost":
+                mlflow.sklearn.log_model(model, "model")
+            else:
+                mlflow.xgboost.log_model(model, "model")
+   
     return model
